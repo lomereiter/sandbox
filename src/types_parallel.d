71c902979765524941d4453b9e7646324338133d
Artem Tarasov <lomereiter@gmail.com>
2012-04-17 01:45:25 +0400

    fix

--- src/types_parallel.d
+++ src/types_parallel.d
@@ -4,21 +4,37 @@ import std.array;
 import std.parallelism;
 import core.sync.mutex;
 
-void main(string[] args) {
-    __gshared uint[string] occurrences;
-      
-    Mutex m = new Mutex();
+synchronized class OccurrenceCounter {
+    private shared(uint[string]) occurrences;
 
+    synchronized public void increment(string str) {
+        occurrences[str] += 1;
+    }
+
+    synchronized public @property auto keys() {
+        return occurrences.keys;
+    }
+
+    synchronized public uint opIndex(string s) const {
+        return occurrences[s];
+    }
+}
+
+
+void main(string[] args) {
+    auto occurrences = new shared(OccurrenceCounter);
     auto fin = File("../data/Cucumber_v2i.gff3");
-  
-    foreach(line; taskPool.parallel(fin.byLine())) {
-        if (line.length == 0 || line[0] == '#') {
-            continue;
-        }
-        auto type = array(splitter(line.dup, '\t'))[2].idup;
-        synchronized(m) { // how the hell to synchronize the access?????
-            occurrences[type] += 1;
-        }
+
+    string[] lines = [];
+    foreach(line; fin.byLine())
+        lines ~= line.idup;
+
+    foreach(line; taskPool.parallel(lines, 500)) {
+            if (line.length == 0 || line[0] == '#') {
+                continue;
+            }
+            string type = array(splitter(line.dup, '\t'))[2].idup;
+            occurrences.increment(type);
     }
 
     foreach(type; occurrences.keys) {
