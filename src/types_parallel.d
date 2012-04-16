66b38a0e8f3d1a742bb39e95ca8c465dd4c218e4
Artem Tarasov <lomereiter@gmail.com>
2012-04-16 20:45:10 +0400

    D: new wtf - synchronization

--- src/types_parallel.d
+++ src/types_parallel.d
@@ -2,18 +2,23 @@ import std.stdio;
 import std.algorithm;
 import std.array;
 import std.parallelism;
+import core.sync.mutex;
 
 void main(string[] args) {
-    uint[string] occurrences;
+    __gshared uint[string] occurrences;
+      
+    Mutex m = new Mutex();
 
     auto fin = File("../data/Cucumber_v2i.gff3");
-   
+  
     foreach(line; taskPool.parallel(fin.byLine())) {
         if (line.length == 0 || line[0] == '#') {
             continue;
         }
-        auto type = array(splitter(line, '\t'))[2].idup;
-        occurrences[type] += 1;
+        auto type = array(splitter(line.dup, '\t'))[2].idup;
+        synchronized(m) { // how the hell to synchronize the access?????
+            occurrences[type] += 1;
+        }
     }
 
     foreach(type; occurrences.keys) {
